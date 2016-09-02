e794cad098518b4939dc86de0ae781f3eb562cf6
Yoan Mollard <yoan.mollard@inria.fr>
2016-09-02 17:54:55 +0200

    Add setup.py

---
+++ python/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+setup(
+    name='dmp_bbo',
+    version='1.0.0',
+    license="GNU General Public License 2",
+    description="Python library for Function Approximation, Dynamical Movement Primitives, and Black-Box Optimization",
+    url='https://github.com/stulp/dmpbbo',
+    author="Stulp",
+    packages=find_packages(),
+)
